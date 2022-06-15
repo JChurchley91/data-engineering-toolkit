b00b77f15aab2d271372012f10fb1dac07896015
JChurchley91 <joseph.churchley@outlook.com>
2022-06-15 19:22:15 +0100

    basic pipeline done..

--- data-pipelines/data_pipelines/pipelines/csv_pipeline.py
+++ data-pipelines/data_pipelines/pipelines/csv_pipeline.py
@@ -1,29 +1,67 @@
 from pandas import DataFrame
+from data_pipelines.utils.yaml_loader import YamlLoader
+from boto3 import session
+
 import pandas as pd
+import os
 
 
 class csvPipeline:
-    def __init__(self, job_config, digital_ocean_config):
+    def __init__(self, job_config):
+        self.yaml_loader = YamlLoader()
         self.job_config = job_config
-        self.digital_ocean_config = digital_ocean_config
+        self.source_job_name = self.job_config['source_job_name']
+        self.source_job_id = self.job_config['source_job_id']
+        self.source_folder_name = self.job_config['source_folder_name']
+        self.source_file_name = self.job_config['source_file_name']
+        self.target_folder_name = self.job_config['target_file_path_folder_name']
+        self.target_file_name = self.job_config['target_file_path_file_name']
 
-    @staticmethod
-    def load_data_from_landing(job_config):
-        df = pd.read_csv(f"../data/landing/{job_config['source_folder_name']}/{job_config['source_file_name']}")
+    def get_digital_ocean_config(self) -> list:
+        config = self.yaml_loader.load_yaml_file('../config/digital_ocean.yaml')
+        return config
+
+    def load_data_from_landing(self) -> DataFrame:
+        df = pd.read_csv(f"../data/landing/{self.source_folder_name}/{self.source_file_name}")
         return df
 
     def write_data_locally(self, df):
+        df.to_csv(self.target_file_name, header=True, index=False)
+        return None
+
+    def perform_quality_checks(self):
         pass
 
-    def perform_quality_checks(self, job_config):
-        pass
+    def write_data_to_target(self):
+        digital_ocean_config = self.get_digital_ocean_config()
+        boto_session = session.Session()
+        boto_client = boto_session.client(
+            "s3",
+            region_name=f"{digital_ocean_config['region_name']}",
+            endpoint_url=f"{digital_ocean_config['endpoint_url']}",
+            aws_access_key_id=f"{digital_ocean_config['access_key_id']}",
+            aws_secret_access_key=f"{digital_ocean_config['secret_access_key']}",
+        )
+        boto_client.upload_file(self.source_file_name, self.target_folder_name, self.target_file_name)
+        return None
 
-    def write_data_to_target(self, df):
-        pass
+    def delete_local_data(self):
+        if os.path.exists(self.source_file_name) and os.path.isfile(self.source_file_name):
+            os.remove(self.source_file_name)
 
-    def delete_local_data(self, job_config):
+    def write_pipeline_logs(self):
         pass
 
     def execute_pipeline(self):
-        print(self.load_data_from_landing(self.job_config))
+        data_found = False
 
+        try:
+            df = self.load_data_from_landing()
+            data_found = True
+        except FileNotFoundError:
+            print('Data not found!')
+
+        if data_found:
+            self.write_data_locally(df)
+            self.write_data_to_target()
+            self.delete_local_data()
--- data-pipelines/data_pipelines/source_jobs/load_penguins.py
+++ data-pipelines/data_pipelines/source_jobs/load_penguins.py
@@ -1,12 +1,5 @@
 from data_pipelines.utils.yaml_loader import YamlLoader
 from data_pipelines.pipelines.csv_pipeline import csvPipeline
-from boto3 import session
-
-
-def get_digital_ocean_config() -> list:
-    yaml_loader = YamlLoader()
-    config = yaml_loader.load_yaml_file('../config/digital_ocean.yaml')
-    return config
 
 
 def get_job_config() -> list:
@@ -15,25 +8,6 @@ def get_job_config() -> list:
     return config
 
 
-def write_penguins_to_target() -> None:
-    """
-    Initiate a boto session and boto client.
-    Upload penguins.csv to lemonheadwizards landing.
-    Delete the csv file from local storage.
-    """
-    digit_ocean_config = get_digital_ocean_config()
-    boto_session = session.Session()
-    boto_client = boto_session.client(
-        "s3",
-        region_name=f"{digit_ocean_config['region_name']}",
-        endpoint_url=f"{digit_ocean_config['endpoint_url']}",
-        aws_access_key_id=f"{digit_ocean_config['aws_access_key_id']}",
-        aws_secret_access_key=f"{digit_ocean_config['aws_secret_access_key']}",
-    )
-    boto_client.upload_file("penguins.csv", "landing", "penguins.csv")
-    return None
-
-
-digital_ocean_config = get_digital_ocean_config()
 job_config = get_job_config()
-pipeline = csvPipeline(job_config, digital_ocean_config).execute_pipeline()
+pipeline = csvPipeline(job_config)
+pipeline.execute_pipeline()
