79d61820dff5a3e36742b8f9d14c2d40b0e15d01
JChurchley91 <joseph.churchley@outlook.com>
2022-06-18 19:28:40 +0100

    hotel bookings added

--- data-pipelines/repository.py
+++ data-pipelines/repository.py
@@ -2,9 +2,10 @@ from dagster import repository
 
 from data_pipelines.source_jobs.load_penguins import load_penguins
 from data_pipelines.source_jobs.load_seoul_bikes import load_seoul_bikes
+from data_pipelines.source_jobs.load_hotel_bookings import load_hotel_bookings
 
 
 @repository
 def source_load_jobs():
-    jobs = [load_penguins, load_seoul_bikes]
+    jobs = [load_penguins, load_seoul_bikes, load_hotel_bookings]
     return jobs
